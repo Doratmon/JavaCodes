81c844bdca478e9f55980793670e7465a6d12869
White-1997 <31155098+White-1997@users.noreply.github.com>
2018-04-03 13:12:46 +0800

    2018/4/3

---
+++ day04/01_方法定义与调用/MethodDemo.java
@@ -0,0 +1,16 @@
+public class MethodDemo 
+{
+	static void doWork()
+	{
+		System.out.println("Hello World!");
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println("´úÂëÆ¬¶ÎA");
+		MethodDemo.doWork();
+		System.out.println("´úÂëÆ¬¶ÎB");
+		MethodDemo.doWork();
+		System.out.println("´úÂëÆ¬¶ÎC");
+		MethodDemo.doWork();
+	}
+}
