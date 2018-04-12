9b874c1ed90fc4e5a32d8c5a2a40057ec4cc994c
White-1997 <31155098+White-1997@users.noreply.github.com>
2018-04-12 10:40:20 +0800

    2018/4/12

---
+++ day04/08_数组常见两类异常/ArrayExceptionDemo.java
@@ -0,0 +1,18 @@
+//数组空指针异常与越界异常
+public class  ArrayExceptionDemo
+{
+	public static void main(String[] args) 
+	{
+		/*
+		//数组还未初始化就操作数组：NullPointerException
+		int[] nums = null;
+		System.out.println(nums.length);
+		*/
+
+		/*
+		//数组越界异常：ArrayIndexOutOfBoundsException: 5
+		int[] nums = new int[]{1, 3, 5, 7};
+		System.out.println(nums[5]);
+		*/
+	}
+}
