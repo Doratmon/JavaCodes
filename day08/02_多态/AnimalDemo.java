8b3f29717f3b53fe1d3daeb93fcc07657c061167
White-1997 <31155098+White-1997@users.noreply.github.com>
2018-05-26 00:17:26 +0800

    2018/5/26

--- day08/02_多态/AnimalDemo.java
+++ day08/02_多态/AnimalDemo.java
@@ -12,6 +12,24 @@ class Dog extends Animal
 		System.out.println("吃骨头");
 	}
 }
+class Person//饲养员
+{
+	//没有使用多态时
+	/*void feed(Dog d)
+	{
+		d.eat();
+	}
+	void feed(Cat c)
+	{
+		c.eat();
+	}
+	*/
+	//使用多态,一个方法解决，真实类型由运行类型决定，即子类决定
+	void feed(Animal a)
+	{
+		a.eat();
+	}
+}
 class Cat extends Animal
 {
 	void eat()
@@ -24,9 +42,14 @@ public class AnimalDemo
 	public static void main(String[] args) 
 	{
 		//多态,编译类型和运行类型不相同，以下这种情况是编译类型是运行类型的父类
-		Animal a = new Dog();
-		Animal b = new Cat();
-		a.eat();
-		b.eat();
+		//Animal a = new Dog();
+		//Animal b = new Cat();
+		Dog d = new Dog();
+		Cat c = new Cat();
+		Person p = new Person();
+		p.feed(d);
+		p.feed(c);
+
+		
 	}
 }
