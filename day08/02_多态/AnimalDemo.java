636e7de56fd2b7e36f8dee2dd27cdaf7c971a555
Andy <1609586673@qq.com>
2018-05-26 18:01:09 +0800

    2018/5/26

--- day08/02_多态/AnimalDemo.java
+++ day08/02_多态/AnimalDemo.java
@@ -12,6 +12,21 @@ class Dog extends Animal
 	{
 		System.out.println("吃骨头");
 	}
+	void watch()
+	{
+		System.out.println("看门");
+	}
+}
+class Cat extends Animal
+{
+	void eat()
+	{
+		System.out.println("吃鱼");
+	}
+	void catchMouse()
+	{
+		System.out.println("抓老鼠");	
+	}
 }
 class Person//饲养员
 {
@@ -26,26 +41,37 @@ class Person//饲养员
 	}
 	*/
 	//多态方法，编译类型是运行类型的父类，a由子类实际决定
-	void feed(Animal a)
+	void feed(Animal a)//Animal a = new Dog()
 	{
+		System.out.println("feeding");
 		a.eat();
+		//Dog  d = (Dog)a;
+		//boolean b = A instanceof B判断对象A是不是类B的实例
+		if(a instanceof Dog)
+		{
+			Dog d = (Dog)a;
+			d.watch();
+		}else if(a instanceof Cat)
+		{
+			Cat c = (Cat)a;
+			c.catchMouse();
+			
+		}
+		
 	}
 }
-class Cat extends Animal
-{
-	void eat()
-	{
-		System.out.println("吃鱼");
-	}
-}
+
 public class AnimalDemo 
 {
 	public static void main(String[] args) 
 	{
 
-		//编译类型是运行类型的父类
+		//编译类型是运行类型的父类,真实类型是运行类型，即子类
 		//Animal a = new Dog();
 		//Animal b = new Cat();
+		//a.eat();
+		//b.eat();
+
 		Dog d = new Dog();
 		Cat c = new Cat();
 		Person p = new Person();
@@ -53,12 +79,13 @@ public class AnimalDemo
 		p.feed(c);
 
 		
-
-		//
-		Animal a = new Dog();
-		Animal b = new Cat();
-		a.eat();
-		b.eat();
-
+		Object obj = "ABC";//多态
+		System.out.println(obj instanceof Object);//true
+		System.out.println(obj instanceof String);//true
+		
+		//但是一般只想判断该对象是不是运行类型的实例,gecClass()获取该对象的真实类型，XX.class后面会讲
+		System.out.println(obj.getClass() == Object.class);//false
+		System.out.println(obj.getClass() == String.class);//true
+		
 	}
 }
