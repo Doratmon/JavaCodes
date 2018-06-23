6ff4dc4a01cb0f57fb0a7638447b0f2b13b8c441
Ethan <beyondself5@gmail.com>
2018-06-23 23:08:37 +0800

    2018/6/23

---
+++ Test/src/com/_520it/eclipse/ArrayUtil.java
@@ -0,0 +1,27 @@
+package com._520it.eclipse;
+/**
+ * 数组工具类：定义了操作数组的各种方法，如排序、查找等
+ * @author Ethan
+ * @since 拳皇游戏1.0
+ * @see 另请参见 https://www.badiu.com
+ *
+ */
+public class ArrayUtil {
+	/**
+	 *int类型数组升序排列操作
+	 * @param arr 进行排序的数组
+	 */
+	public static void sort(int[] arr){
+		System.out.println("s升序排序");
+		
+	}
+	/**
+	 *int类型数组进行二分查找
+	 * @param arr 查找的数组
+	 * @return 如果找到该元素则返回索引值，否则返回-1
+	 */
+	public static int binarySearch(int[] arr, int key){
+		return -1;
+	}
+
+}
